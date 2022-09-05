b20d6ac6a8da7b6606a15b62ab95886654686ace
amrendra <amerendra9062@gmail.com>
2022-09-05 15:30:38 +0530

    step 3 done

--- factorial.cpp
+++ factorial.cpp
@@ -1,14 +1,8 @@
 #include "functions.h"
 
 int factorial(int n){
-    if(n<0){
-	printf("Enter a positive number : ");
-	int x;
-	cin>>x;
-	n = x;
-    }
-    if(n!=2){
-	return((n+1) * factorial(n-1));
-    }
-    else return 1;
+    if(n<0)
+	    return -1;
+    if(n==0) return 1;
+	return((n) * factorial(n-1));
 }
